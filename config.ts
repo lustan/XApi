7af9c519b35e1d979db41b42ec5376518ccafacd
Senee Lu <luson009@gmail.com>
2026-06-11 00:25:39 +0800

    curl x-www-form-urlencoded导入自动decode
    Signed-off-by: 卢声康 <shengkang.lu@dmall.com>

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 
 export const APP_CONFIG = {
-  VERSION: '1.1.1',
+  VERSION: '2.1.0',
   GITHUB_URL: 'https://github.com/lustan/XApi',
   FEEDBACK_URL: 'https://github.com/lustan/XApi/issues',
 };
